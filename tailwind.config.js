ad01c0b779d21a1387c6e126cc4b36741d66418e
vijayan24216 <24216.ad@rmkcet.ac.in>
2025-06-13 08:56:17 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./ssrc/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
